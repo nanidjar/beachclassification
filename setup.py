d3f20415d20e3a1819447ec45843a74709cf5fb9
nanidjar <50150002+nanidjar@users.noreply.github.com>
2020-06-09 13:28:29 -0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import pathlib
+from setuptools import setup, find_packages
+
+setup(
+    
+    name='beach_classification',
+    url='https://github.com/nanidjar/beach_classification',
+    author='Niv Anidjar',
+    author_email='nanidjar@ucsd.edu',
+    
+    install_requires=['numpy', 'sklearn', 'os', 'pickle', 'scipy', 'laspy', 'skimage', 'utm',
+                      'pyyaml','numba','tqdm'],
+    packages=find_packages(),
+    version='0.0.1',
+    
+    license='GPL-3.0',
+    description='process and label beach LIDAR surveys',
+    
+    long_description=(pathlib.Path(__file__).parent / "README.md"),
+    long_description_content_type = "text/markdown"
+)
