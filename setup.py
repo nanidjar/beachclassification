3b2bf49d20addd31187efc2c85caa98cfb78d812
nanidjar <50150002+nanidjar@users.noreply.github.com>
2020-06-09 13:59:05 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Niv Anidjar',
     author_email='nanidjar@ucsd.edu',
     
-    install_requires=['numpy', 'sklearn', 'os', 'pickle', 'scipy', 'laspy', 'skimage', 'utm',
+    install_requires=['numpy', 'sklearn', 'scipy', 'laspy', 'skimage', 'utm',
                       'pyyaml','numba','tqdm'],
     packages=find_packages(),
     version='0.0.1',
